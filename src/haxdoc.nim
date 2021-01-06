60a4497b143cb877b261cb3a3c06342b998b30cc
haxscramper <haxscramper@gmail.com>
2021-01-06 22:36:59 +0300

    [FEATURE] Find symbol at position
    - ADDED ::
      - WIP find symbol at particular position in original file. Not tested
        with templates/macro yet.

--- src/haxdoc.nim
+++ src/haxdoc.nim
@@ -1,64 +1,147 @@
 {.experimental: "caseStmtMacros".}
 
-import std/[os, strformat]
+import std/[os, strformat, with]
 import hmisc/other/oswrap
+import hnimast except nkStrKinds
 import fusion/matching
 import compiler /
   [ idents, options, modulegraphs, passes, lineinfos, sem, pathutils, ast,
-    astalgo, modules, condsyms, passaux
+    astalgo, modules, condsyms, passaux, llstream, parser
   ]
 
-let file = "/tmp/ee.nim"
+{.push inline.}
 
-file.writeFile("""
-proc test*(arg: int) =
-  ## Documentation for proc `test`
-  ##
-  ## - @arg{arg} :: Input argument
-  echo arg
-""")
+func `-`*[E](s1: set[E], s2: E): set[E] = s1 - {s2}
+func `-=`*[E](s1: var set[E], v: E | set[E]) = (s1 = s1 - {v})
+
+{.pop.}
 
 
-var graph: ModuleGraph = block:
+converter toAbsoluteDir*(dir: AbsDir): AbsoluteDir =
+  AbsoluteDir(dir.string)
+
+converter toAbsoluteFile*(file: AbsFile): AbsoluteFile =
+  AbsoluteFile(file.string)
+
+proc parsePNode(file: AbsFile): PNode =
+  let cache: IdentCache = newIdentCache()
+  let config: ConfigRef = newConfigRef()
+  var pars: Parser
+
+  openParser(
+    p = pars,
+    filename = file,
+    inputStream = llStreamOpen(file, fmRead),
+    cache = cache,
+    config = config
+  )
+
+  result = parseAll(pars)
+  closeParser(pars)
+
+proc newModuleGraph(file: AbsFile): ModuleGraph =
   var
     cache: IdentCache = newIdentCache()
     config: ConfigRef = newConfigRef()
 
   let path = ~".choosenim/toolchains/nim-1.4.0/lib"
 
-  config.libpath = AbsoluteDir(path)
-  config.searchPaths.add config.libpath
-  config.projectFull = AbsoluteFile(file)
+  with config:
+    verbosity = 0
+    libpath = AbsoluteDir(path)
+    cmd = cmdIdeTools
+
+  config.options -= optHints
+
+  config.searchPaths.add @[
+    config.libpath,
+    path / "pure",
+    path / "pure" / "collections",
+    path / "impure",
+    path / "std",
+    path / "core",
+    path / "posix"
+  ]
+
+  config.projectFull = file
+
+
+  wantMainModule(config)
+
   initDefines(config.symbols)
 
-  newModuleGraph(cache, config)
-
+  return newModuleGraph(cache, config)
 
 type
   CustomContext = ref object of PPassContext
     module: PSym
 
+proc registerToplevel(ctx: CustomContext, node: PNode) =
+  discard
+
 proc passOpen(graph: ModuleGraph; module: PSym): PPassContext =
   CustomContext(module: module)
 
 proc passNode(c: PPassContext, n: PNode): PNode =
-  let c = CustomContext(c)
   result = n
-  if sfMainModule in c.module.flags:
-    case n:
-      of ProcDef[^1 is [any @comments is CommentStmt()]]:
-        for comment in comments:
-          echo comment.comment
-
-    # [any @comments is CommentStmt()] := n[^1]
-    # debug(n)
-    # echo n.comment
-    # echo n.kind
+  if sfMainModule in CustomContext(c).module.flags:
+    registerToplevel(CustomContext(c), n)
 
 proc passClose(graph: ModuleGraph; p: PPassContext, n: PNode): PNode =
   discard
 
 
+
+# proc isTracked*(current, trackPos: TLineInfo, tokenLen: int): bool =
+
+proc findNode*(n: PNode; trackPos: TLineInfo): PSym =
+  if n.kind == nkSym:
+    if n.info.fileIndex == trackPos.fileIndex and
+       n.info.line == trackPos.line:
+
+      if trackPos.col in (n.info.col .. n.info.col + n.sym.name.s.len - 1):
+        return n.sym
+
+  else:
+    for i in 0 ..< safeLen(n):
+      let res = findNode(n[i], trackPos)
+      if res != nil:
+        return res
+
+proc symFromInfo(graph: ModuleGraph; trackPos: TLineInfo): PSym =
+  let m = graph.getModule(trackPos.fileIndex)
+  if m != nil and m.ast != nil:
+    result = findNode(m.ast, trackPos)
+
+proc annotateAst(graph: ModuleGraph, node: PNode) =
+  case node.kind:
+    of nkStrKinds, nkIntKinds, nkFloatKinds:
+      discard
+
+    of nkIdent:
+      let sym = graph.symFromInfo(node.info)
+      echo "------"
+      if not sym.isNil:
+        echo sym.ast
+
+    else:
+      for subnode in items(node):
+        annotateAst(graph, subnode)
+
+let file = AbsFile("/tmp/file.nim")
+
+file.writeFile("""
+proc hello(): int = 12
+
+let val = hello()
+
+echo val
+""")
+
+var graph: ModuleGraph = newModuleGraph(file)
+let fileAst: PNode = parsePNode(file)
 registerPass(graph, semPass)
 registerPass(graph, makePass(passOpen, passNode, passClose))
 compileProject(graph)
+
+annotateAst(graph, fileAst)
