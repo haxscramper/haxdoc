857c72c2ab42f6160ae8e5704257f968dd2e3fa7
haxscramper <haxscramper@gmail.com>
2021-01-31 21:13:25 +0300

    [WIP][FEATURE] Documentation IR outline

---
+++ src/haxdoc/docentry.nim
@@ -0,0 +1,75 @@
+import haxorg/semorg
+
+type
+  DocEntryKind = enum
+    dekProc
+    dekFunc
+    dekMacro
+    dekMethod
+    dekTemplate
+    dekIterator
+
+    dekObject
+    dekException
+    dekConcept
+    dekEnum
+    dekEffect
+    dekAlias
+    dekDistinctAlias
+
+    dekGlobalConst
+    dekGlobalVar
+    dekGlobalLet
+
+    dekArgument
+    dekField
+
+const
+  dekProcKinds* = {
+    dekProc,
+    dekFunc,
+    dekMacro,
+    dekMethod,
+    dekTemplate,
+    dekIterator
+  }
+
+  dekNewtypeKinds* = {
+    dekObject,
+    dekException,
+    dekConcept,
+    dekEnum,
+    dekEffect,
+    dekDistinctAlias
+  }
+
+type
+  DocText = ref object of SemOrg
+
+  DocType = object
+
+  DocEntry = ref object
+    plainName*: string
+    genParams*: seq[DocType]
+    doctext*: DocText
+    case kind*: DocEntryKind
+      of dekProcKinds:
+        arguments*: seq[DocEntry]
+        prReturn*: DocType
+
+      of dekArgument:
+        argName*: string
+        argType*: DocType
+
+      of dekField:
+        fieldName*: string
+        fieldType*: DocType
+
+      of dekObject, dekException:
+        discard
+
+      of dekEffect:
+        discard
+
+      else:
+        discard
