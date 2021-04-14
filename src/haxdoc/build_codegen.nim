a65b17cea7fb99ea28e1accd457289b9bc8e19fb
haxscramper <haxscramper@gmail.com>
2021-04-14 19:07:03 +0300

    [CHECKPOINT]

--- src/haxdoc/build_codegen.nim
+++ src/haxdoc/build_codegen.nim
@@ -1,22 +1,32 @@
 import nimtraits/xml_to_types
-import hmisc/other/[oswrap]
+import hmisc/other/[oswrap, hshell]
 import hnimast
+import hmisc/hdebug_misc
 
 const dir = currentSourceDir()
 
 proc doxygenXmlGenerate() =
-  generateForXsd(cwd() /. "extract/doxygen_compound.xsd").writeXsdGenerator(
-    cwd() /. "extract/doxygen_compound.nim")
+  let compound = cwd() /. "extract/doxygen_compound.nim"
+  generateForXsd(cwd() /. "extract/doxygen_compound.xsd").
+    writeXsdGenerator(compound)
 
-  generateForXsd(cwd() /. "extract/doxygen_index.xsd").writeXsdGenerator(
-    cwd() /. "extract/doxygen_index.nim")
+  let index = cwd() /. "extract/doxygen_index.nim"
+  generateForXsd(cwd() /. "extract/doxygen_index.xsd").
+    writeXsdGenerator(index)
+
+  execShell shellCmd(nim, check, errormax = 2, $compound)
+  execShell shellCmd(nim, check, errormax = 2, $index)
 
   echo "Doxygen generate done"
+
+  execShell shellCmd(nim, r, "extract/from_doxygen_xml.nim")
+  echo "Compilation ok"
 
 
 
 when isMainModule:
   if paramCount() == 0:
+    startHax()
     doxygenXmlGenerate()
 
   else:
